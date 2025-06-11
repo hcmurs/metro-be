4debab7068fab95e92699c7ee7ad9c2341801877
NguyenNgoAnhhTu <anhtu113kx@gmail.com>
2025-06-11 16:57:25 +0700

    fix(station): fix schedule

--- services/station-service/src/main/java/com/example/stationservice/dto/SchedulesRequest.java
+++ services/station-service/src/main/java/com/example/stationservice/dto/SchedulesRequest.java
@@ -1,6 +1,7 @@
 package com.example.stationservice.dto;
 
 import com.example.stationservice.model.Schedules;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -13,8 +14,10 @@ public class SchedulesRequest {
     @NotNull(message = "Station ID cannot be null")
     private Long stationId;
     @NotNull(message = "Time of arrival cannot be null")
+    @JsonFormat(pattern = "HH:mm",shape = JsonFormat.Shape.STRING)
     private LocalTime timeArrival;
     @NotNull(message = "Time of departure cannot be null")
+    @JsonFormat(pattern = "HH:mm",shape = JsonFormat.Shape.STRING)
     private LocalTime timeDeparture;
     @NotNull(message = "Description cannot be null")
     @Size(max = 500, message = "Description cannot exceed 500 characters")
