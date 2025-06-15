1d534964c22184d2173c292e35bae40e5a46f019
lcaohoanq <hoangclw@gmail.com>
2025-06-16 01:33:02 +0700

    fix: fix cd

--- postgres_init.sql
+++ postgres_init.sql
@@ -2,16 +2,16 @@
 
 -- Create User Service DB
 DROP DATABASE IF EXISTS userdb;
-CREATE DATABASE userdb WITH OWNER = admin ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
+CREATE DATABASE userdb WITH OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
 
 -- Create Curriculum Service DB
 DROP DATABASE IF EXISTS orderdb;
-CREATE DATABASE orderdb WITH OWNER = admin ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
+CREATE DATABASE orderdb WITH OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
 
 -- Create Curriculum Service DB
 DROP DATABASE IF EXISTS stationdb;
-CREATE DATABASE stationdb WITH OWNER = admin ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
+CREATE DATABASE stationdb WITH OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
 
 -- Create Curriculum Service DB
 DROP DATABASE IF EXISTS ticketdb;
-CREATE DATABASE ticketdb WITH OWNER = admin ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
+CREATE DATABASE ticketdb WITH OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
--- services/order-service/src/main/java/com/example/cronjob/OrderApplication.java
+++ services/order-service/src/main/java/com/example/cronjob/OrderApplication.java
@@ -19,7 +19,7 @@ public class OrderApplication {
 
         var env = context.getEnvironment();
         var activeProfiles = env.getActiveProfiles();
-        if (!Arrays.asList(activeProfiles).contains("docker")) {
+        if (!Arrays.asList(activeProfiles).contains("docker") && !Arrays.asList(activeProfiles).contains("test") && !Arrays.asList(activeProfiles).contains("zimaos")) {
             JavaBrowserLauncher.openHomePage("http://localhost:4008/swagger-ui.html");
         }
     }
