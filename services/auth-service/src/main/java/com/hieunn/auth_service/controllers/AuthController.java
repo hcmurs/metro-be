011f8991144f8cfed1c814dd295b23a56bdcea4a
NguyenNgoAnhhTu <anhtu113kx@gmail.com>
2025-06-19 09:57:45 +0700

    fix: login gg

--- services/auth-service/src/main/java/com/hieunn/auth_service/controllers/AuthController.java
+++ services/auth-service/src/main/java/com/hieunn/auth_service/controllers/AuthController.java
@@ -1,43 +1,53 @@
-package com.hieunn.auth_service.controllers;
+    package com.hieunn.auth_service.controllers;
 
-import com.hieunn.auth_service.dtos.requests.LocalLoginRequest;
-import com.hieunn.auth_service.dtos.responses.ApiResponse;
-import com.hieunn.auth_service.dtos.responses.UserDto;
-import com.hieunn.auth_service.dtos.requests.RegisterRequest;
-import com.hieunn.auth_service.services.AuthService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+    import com.hieunn.auth_service.dtos.requests.GoogleLoginRequest;
+    import com.hieunn.auth_service.dtos.requests.LocalLoginRequest;
+    import com.hieunn.auth_service.dtos.responses.ApiResponse;
+    import com.hieunn.auth_service.dtos.responses.TokenResponse;
+    import com.hieunn.auth_service.dtos.responses.UserDto;
+    import com.hieunn.auth_service.dtos.requests.RegisterRequest;
+    import com.hieunn.auth_service.services.AuthService;
+    import jakarta.servlet.http.HttpServletRequest;
+    import jakarta.servlet.http.HttpServletResponse;
+    import jakarta.validation.Valid;
+    import lombok.AccessLevel;
+    import lombok.RequiredArgsConstructor;
+    import lombok.experimental.FieldDefaults;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.bind.annotation.RequestBody;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/auth")
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-@RequiredArgsConstructor
-public class AuthController {
-    AuthService authService;
+    @RestController
+    @RequestMapping("/auth")
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    @RequiredArgsConstructor
+    public class AuthController {
+        AuthService authService;
 
-    @PostMapping("/logout")
-    public ResponseEntity<ApiResponse<Void>> logout(HttpServletRequest request, HttpServletResponse response) {
-        authService.logout(request, response);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(ApiResponse.success("Logout successfully"));
+        @PostMapping("/logout")
+        public ResponseEntity<ApiResponse<Void>> logout(HttpServletRequest request, HttpServletResponse response) {
+            authService.logout(request, response);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(ApiResponse.success("Logout successfully"));
+        }
+
+        @PostMapping("/local-login")
+        public ResponseEntity<ApiResponse<UserDto>> localLogin(@RequestBody LocalLoginRequest localLoginRequest) {
+            ApiResponse<UserDto> apiResponse = authService.processLocalLogin(localLoginRequest);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(apiResponse);
+        }
+
+        @PostMapping("/oauth2/google")
+        public ResponseEntity<ApiResponse<TokenResponse>> authenticateWithGoogle(@RequestBody GoogleLoginRequest request) {
+            ApiResponse<TokenResponse> response = authService.processGoogleLogin(request.getIdToken());
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(response);
+        }
     }
-
-    @PostMapping("/local-login")
-    public ResponseEntity<ApiResponse<UserDto>> localLogin(@RequestBody LocalLoginRequest localLoginRequest) {
-        ApiResponse<UserDto> apiResponse = authService.processLocalLogin(localLoginRequest);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(apiResponse);
-    }
-}
--- services/auth-service/src/main/java/com/hieunn/auth_service/services/AuthService.java
+++ services/auth-service/src/main/java/com/hieunn/auth_service/services/AuthService.java
@@ -2,6 +2,7 @@ package com.hieunn.auth_service.services;
 
 import com.hieunn.auth_service.dtos.requests.LocalLoginRequest;
 import com.hieunn.auth_service.dtos.responses.ApiResponse;
+import com.hieunn.auth_service.dtos.responses.TokenResponse;
 import com.hieunn.auth_service.dtos.responses.UserDto;
 import com.hieunn.auth_service.dtos.requests.RegisterRequest;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,4 +11,6 @@ import jakarta.servlet.http.HttpServletResponse;
 public interface AuthService {
     void logout(HttpServletRequest request, HttpServletResponse response);
     ApiResponse<UserDto> processLocalLogin(LocalLoginRequest localLoginRequest);
+    ApiResponse<TokenResponse> processGoogleLogin(String idToken);
+
 }
