a070aa0105860fbfe771ad7df2bbecf0f996d624
NguyenNgoAnhhTu <anhtu113kx@gmail.com>
2025-06-24 08:01:40 +0700

    fix: yml auth

--- services/auth-service/src/main/java/com/hieunn/auth_service/services/AuthService.java
+++ services/auth-service/src/main/java/com/hieunn/auth_service/services/AuthService.java
@@ -2,15 +2,15 @@ package com.hieunn.auth_service.services;
 
 import com.hieunn.auth_service.dtos.requests.LocalLoginRequest;
 import com.hieunn.auth_service.dtos.responses.ApiResponse;
-import com.hieunn.auth_service.dtos.responses.TokenResponse;
 import com.hieunn.auth_service.dtos.responses.UserDto;
 import com.hieunn.auth_service.dtos.requests.RegisterRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.util.Map;
+
 public interface AuthService {
     void logout(HttpServletRequest request, HttpServletResponse response);
     ApiResponse<UserDto> processLocalLogin(LocalLoginRequest localLoginRequest);
-    ApiResponse<TokenResponse> processGoogleLogin(String idToken);
-
+    Map<String, Object> processMobileOAuth2Login(String code, String registrationId);
 }
