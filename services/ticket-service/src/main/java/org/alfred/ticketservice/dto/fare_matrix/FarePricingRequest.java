079f88f9376a4324129cb2a05635a9123e9437c4
nn-hieu <nn.hieu0701@gmail.com>
2025-07-20 23:10:15 +0700

    Change logic validate

--- services/ticket-service/src/main/java/org/alfred/ticketservice/dto/fare_matrix/FarePricingRequest.java
+++ services/ticket-service/src/main/java/org/alfred/ticketservice/dto/fare_matrix/FarePricingRequest.java
@@ -3,18 +3,19 @@ package org.alfred.ticketservice.dto.fare_matrix;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public record FarePricingRequest(
-        @Positive(message = "Min distance must be a positive number")
+        @PositiveOrZero(message = "Min distance must be a positive number")
         @NotNull(message = "Min distance must be a positive number")
-    int minDistanceKm,
+        int minDistanceKm,
         @Positive(message = "Max distance must be a positive number")
         @NotNull(message = "Max distance must be a positive number")
-    int maxDistanceKm,
-    @Positive(message = "price must be a positive number")
-    @NotNull(message = "price must be a valid number")
-    float price,
+        int maxDistanceKm,
+        @Positive(message = "price must be a positive number")
+        @NotNull(message = "price must be a valid number")
+        float price,
         @NotNull(message = "isActive must be true or false")
-    boolean isActive
+        boolean isActive
 ) {
 }
