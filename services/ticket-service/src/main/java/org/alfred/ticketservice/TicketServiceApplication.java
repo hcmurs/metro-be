05b90d293cc53a72b77db32335114b6f4c969638
lcaohoanq <hoangclw@gmail.com>
2025-09-22 20:49:33 +0700

    chore: update lint

--- services/ticket-service/src/main/java/org/alfred/ticketservice/TicketServiceApplication.java
+++ services/ticket-service/src/main/java/org/alfred/ticketservice/TicketServiceApplication.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2025 hcmurs. All rights reserved.
- * <p>
+ *
  * Service: Ticket-Service
- * <p>
- * This software is the confidential and proprietary information of hcmurs. You shall not disclose such confidential
- * information and shall use it only in accordance with the terms of the license agreement you entered into with
- * hcmurs.
+ *
+ * This software is the confidential and proprietary information of hcmurs.
+ * You shall not disclose such confidential information and shall use it only in
+ * accordance with the terms of the license agreement you entered into with hcmurs.
  */
 package org.alfred.ticketservice;
 
@@ -25,7 +25,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @BrowserLauncher(value = "http://localhost:4005/swagger-ui.html")
 public class TicketServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TicketServiceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(TicketServiceApplication.class, args);
+  }
 }
