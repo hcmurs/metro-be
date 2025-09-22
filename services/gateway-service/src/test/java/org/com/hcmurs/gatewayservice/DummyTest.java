5b9a3f6d1ebb5cd273dc5199b9d5c195b15538b1
lcaohoanq <hoangclw@gmail.com>
2025-09-22 21:50:13 +0700

    chore: trigger ci

---
+++ services/gateway-service/src/test/java/org/com/hcmurs/gatewayservice/DummyTest.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2025 hcmurs. All rights reserved.
+ *
+ * Service: Gateway-Service
+ *
+ * This software is the confidential and proprietary information of hcmurs.
+ * You shall not disclose such confidential information and shall use it only in
+ * accordance with the terms of the license agreement you entered into with hcmurs.
+ */
+package org.com.hcmurs.gatewayservice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+  @Test
+  void sum() {
+    // arrange
+    var number1 = 1;
+    var number2 = 2;
+
+    // act
+    var result = number1 + number2;
+
+    // assert
+    assertEquals(3, result);
+  }
+}
